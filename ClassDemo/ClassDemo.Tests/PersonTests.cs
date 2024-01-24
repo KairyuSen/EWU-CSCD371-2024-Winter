b40f144ba3b5b204f467d4fb0aacdd8229a2e3ef
Kyle Wong <kwong15@ewu.edu>
2024-01-23 16:09:41 -0800

    My edits in class

--- ClassDemo/ClassDemo.Tests/PersonTests.cs
+++ ClassDemo/ClassDemo.Tests/PersonTests.cs
@@ -11,7 +11,38 @@ public class PersonTests
     [Fact]
     public void Constructor_ValidName_SuccessfulInitialization()
     {
-        Person person = new();
+        Person person = new(); // Simplified new.
+    
     }
 
+    [Fact]
+    public void FirstName_SetInigo_Success()
+    {
+        Person person = new();
+        person.FirstName = "Inigo";
+        Assert.Equal("Inigo", person.FirstName);
+    }
+
+    [Fact]
+    public void Constructor_SetInigo_ThrowNull()
+    {
+        PersonTests? person;
+        Assert.Throws<ArgumentNullException>(
+            () =>
+            {
+                person = new(null!);
+            }
+            );
+    }
+
+    [Fact]
+    public void FirstName_SetNull_Success()
+    {
+        Person person = new();
+     
+        Assert.throws<ArgumentNullException>(
+            () => person.FirstName = null);
+        
+    }
 }
+
--- ClassDemo/ClassDemo/Person.cs
+++ ClassDemo/ClassDemo/Person.cs
@@ -2,5 +2,27 @@
 
 public class Person
 {
+    public Person(string firstName)
+    {
+        FirstName = firstName;
+    }
+    private string _firstName; //Never access this field except from within the
+                               //implementation of the get and set methods.
 
+    public string FirstName
+    {
+        get => _firstName!;
+        set
+        {
+            _firstName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+
+    public string LastName { get; set; }
+
+    public void SetName(string? temp)
+    {
+        _firstName = null; // Wrong
+        FirstName = null; // Right
+    }
 }
